864081145bf1653885351dca25d45d5623d660af
Dmitry Skripak <zverdiman9875@gmail.com>
2018-10-02 20:26:56 +0500

    [Upd] Change routes.

--- app/FileStore/Config.js
+++ app/FileStore/Config.js
@@ -5,12 +5,12 @@ Ext.define('MyApp.FileStore.Config', {
         filesGrid: 'filesGrid'
     },
     urlMethods: {
-        getFilesData: 'http://t-maxim/web/?r=main/get_files_data',
+        getFilesData: 'http://t-maxim/web/?r=main/get-files-data',
         delete: 'http://t-maxim/web/?r=main/delete',
-        createNewFolder: 'http://t-maxim/web/?r=main/new_folder',
+        createNewFolder: 'http://t-maxim/web/?r=main/new-folder',
         rename: 'http://t-maxim/web/?r=main/rename',
-        getFileContent: 'http://t-maxim/web/?r=main/get_content',
-        createOrRewriteFile: 'http://t-maxim/web/?r=main/create_or_rewrite_file',
+        getFileContent: 'http://t-maxim/web/?r=main/get-content',
+        createOrRewriteFile: 'http://t-maxim/web/?r=main/create-or-rewrite-file',
         uploadFile: 'http://t-maxim/web/?r=main/upload'
 
     }
